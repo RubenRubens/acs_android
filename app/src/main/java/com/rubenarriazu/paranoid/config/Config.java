4b5d67797fea5bd31b45381d6b16b2afd03f2ce0
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-05-03 21:26:23 +0200

    Set the IP of the emulator to 10.0.2.2 (alias for localhost)

--- app/src/main/java/com/rubenarriazu/paranoid/config/Config.java
+++ app/src/main/java/com/rubenarriazu/paranoid/config/Config.java
@@ -2,7 +2,7 @@ package com.rubenarriazu.paranoid.config;
 
 public class Config {
     // If you change WEB_SERVER you must changed res/xml/network_security_config.xml as well
-    public static final String WEB_SERVER = "https://acs.rubenarriazu.com/";
+    public static final String WEB_SERVER = "http://10.0.2.2:8000/";
 
     // Encrypted shared preferences to store a username, password and token
     public static final String CREDENTIALS = "credentials.xml";
