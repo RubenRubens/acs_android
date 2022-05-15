1255f00df47a1e7b017ebf2de38b8ec198054303
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-05-15 18:14:20 +0200

    Create a first version to view a user profile

--- app/src/main/java/com/rubenarriazu/paranoid/api/APIClient.java
+++ app/src/main/java/com/rubenarriazu/paranoid/api/APIClient.java
@@ -7,6 +7,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
 
+    public static Retrofit newRetrofit() {
+        return  new Retrofit.Builder()
+                .baseUrl(Config.WEB_SERVER)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(Config.WEB_SERVER)
             .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/rubenarriazu/paranoid/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/rubenarriazu/paranoid/ui/profile/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.rubenarriazu.paranoid.ui.profile;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,48 +17,83 @@ import com.google.android.material.button.MaterialButton;
 import com.rubenarriazu.paranoid.R;
 import com.rubenarriazu.paranoid.api.APIClient;
 import com.rubenarriazu.paranoid.api.Endpoints;
+import com.rubenarriazu.paranoid.api.responses.AccountResponse;
 import com.rubenarriazu.paranoid.api.responses.UserResponse;
 import com.rubenarriazu.paranoid.credentials.Credentials;
 
+import java.io.InputStream;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+/**
+ * Given a user PK, inflates a profile with the information
+ * of that user.
+ */
 public class ProfileFragment extends Fragment {
 
+    private static final String USER_PK = "user_pk";
+
+    private int userPK;
     private TextView name;
     private TextView username;
+    private TextView bio;
     private MaterialButton followersButton;
     private MaterialButton followingButton;
+    private ImageView profilePicture;
+    private ImageView postImage;
+
+    // extracted from https://programmer.group/android-confusion-why-pass-parameters-with-fragment.setarguments-bundle-bundle.html
+    public static Fragment newInstance(int userPK) {
+        var profileFragment = new ProfileFragment();
+        var bundle = new Bundle();
+        bundle.putInt(USER_PK, userPK);
+        profileFragment.setArguments(bundle);
+        return profileFragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        assignUserPK(getArguments());
+        assignViews(v);
+        initProfileInfo(userPK);
         return v;
+    }
+
+    private void assignUserPK(Bundle bundle) {
+        this.userPK = bundle.getInt(USER_PK);
     }
 
     private void assignViews(View v) {
         name = v.findViewById(R.id.text_name);
         username = v.findViewById(R.id.text_username);
+        bio = v.findViewById(R.id.text_bio);
         followersButton = v.findViewById(R.id.button_followers);
         followingButton = v.findViewById(R.id.button_following);
+        profilePicture = v.findViewById(R.id.image_profile);
+        postImage = v.findViewById(R.id.image_post);
     }
 
-    // Set the view name and username
+    // Set the information of the user and its account
     private void setProfileInfo(UserResponse userResponse) {
         String firstName = userResponse.getFirstName();
         String lastName = userResponse.getLastName();
         name.setText(firstName + " " + lastName);
         username.setText("@" + userResponse.getUsername());
+        setProfilePicture(userPK);
+        setBio(userPK);
     }
 
     // Get the username, first name and last name from the API
-    private void initProfileInfo() {
+    private void initProfileInfo(int userPK) {
         var credentials = new Credentials(getContext());
         String token = credentials.getToken();
         Endpoints endpoints = APIClient.retrofit.create(Endpoints.class);
-        Call<UserResponse> call = endpoints.getUser("Token " + token);
+        Call<UserResponse> call = endpoints.getUser("Token " + token, userPK);
         call.enqueue(new Callback<UserResponse>() {
             @Override
             public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
@@ -72,6 +110,58 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onFailure(Call<UserResponse> call, Throwable t) {
+            }
+        });
+    }
+
+    // Sets the profile bio from the userPK
+    private void setBio(int userPK) {
+        var credentials = new Credentials(getContext());
+        String token = credentials.getToken();
+        Endpoints endpoints = APIClient.retrofit.create(Endpoints.class);
+        Call<AccountResponse> call = endpoints.getAccount("Token " + token, userPK);
+        call.enqueue(new Callback<AccountResponse>() {
+            @Override
+            public void onResponse(Call<AccountResponse> call, Response<AccountResponse> response) {
+                if (response.isSuccessful()) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            bio.setText(response.body().getBio());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AccountResponse> call, Throwable t) {
+            }
+        });
+    }
+
+    // Given the userPK, inflates the profile picture for his/her account
+    private void setProfilePicture(int userPK) {
+        var credentials = new Credentials(getContext());
+        String token = credentials.getToken();
+        Endpoints endpoints = APIClient.retrofit.create(Endpoints.class);
+        Call<ResponseBody> call = endpoints.getProfilePicture("Token " + token, userPK);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    InputStream bytes = response.body().byteStream();
+                    Bitmap bitmap = BitmapFactory.decodeStream(bytes);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            profilePicture.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
             }
         });
     }
